ad732b56aee56086f46eeb948c5edef2ec29aa3c
Vivek Anjan <vivekanjankc12345@gmail.com>
2023-07-10 18:17:40 +0530

    done

--- c/main.py
+++ c/main.py
@@ -1,2 +1,22 @@
 from flask import Flask
-from flask_pymongo import
+from flask_pymongo import Pymongo
+from bson.objectid import ObjectId
+app=Flask(__name__)
+app.config["Mongo_URL"]="mangodb://localhost:270017/kaka"
+mango=Pymongo(app)
+data_collection=mango.db.data
+@app.route("/",methods=["GET"])
+def getdata():
+    datas=data_collection.find()
+    data1=[]
+    for data in datas:
+        data["-id"]=str(data["_id"])
+
+@app.route("/add",methods=["POST"])
+def adddata():   
+@app.route("/uddate/<id>",methods=["PUT"])
+def adddata():  
+@app.route("/delete/<id>",methods=["DELETE"])
+def adddata():         
+if_name=="_main":
+app.run()
